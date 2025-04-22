b824fc873e715a12b1f1f80e45492c6578eceb3b
Mahdi-Babaei <mahdibabaei.8626@gmail.com>
2025-04-22 16:11:24 +0330

    init slider

--- content/JS/app.js
+++ content/JS/app.js
@@ -221,9 +221,13 @@ jalaliDatepicker.startWatch({
 // Swiper Slider
 let swiper = new Swiper('.swiper', {
     loop: true,
+    autoplay: true,
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    pagination: {
+        el: '.swiper-pagination',
+      },
   });
